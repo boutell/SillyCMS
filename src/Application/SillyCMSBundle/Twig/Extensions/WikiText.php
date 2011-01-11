9de6e667926e8620167ee2aa5df7b9dee53f55cf
Tom Boutell <tom@punkave.com>
2011-01-11 08:32:27 -0500

    Added a custom Twig extension to support basic Wiki syntax. I was able to do it in a bundle and declare it as a service so that Symfony 2 can find it properly. Note that you have to export your filter function properly - functions get namespaced too. The Wiki syntax is so far not anywhere near as exciting as the technique needed to implement it of courrse (:

---
+++ src/Application/SillyCMSBundle/Twig/Extensions/WikiText.php
@@ -0,0 +1,72 @@
+<?php
+
+/**
+ * This file is part of SillyCMS.
+ *
+ * (c) 2010 P'unk Avenue LLC
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Tom Boutell <tom@punkave.com>
+ * @package SillyCMS
+ * @subpackage Twig
+ */
+  
+namespace Application\SillyCMSBundle\Twig\Extensions;
+use \Twig_Filter_Function;
+use \Twig_Environment;
+use \Twig_Extension;
+
+class WikiText extends Twig_Extension
+{
+    /**
+     * Returns a list of filters.
+     *
+     * @return array
+     */
+    public function getFilters()
+    {
+        return array(
+            // What does needs_environment do exactly?
+            'wiki_text' => new Twig_Filter_Function('\Application\SillyCMSBundle\Twig\Extensions\twig_wikitext_filter', array('is_safe' => array('html'), 'pre_escape' => 'html'))
+        );
+    }
+
+    /**
+     * Name of this extension
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return 'WikiText';
+    }
+}
+
+function twig_wikitext_filter($text)
+{
+    // Multiline regexp won't work with \r in there unless
+    // autodetect is set, if that even works
+    $text = str_replace("\r\n", "\n", $text);
+    $text = preg_replace(
+      array(
+        "/(http\:.*?)([\s\]\)\}]|$)/",
+        "/^= (.*?) =$/m",
+        "/^== (.*?) ==$/m",
+        "/^=== (.*?) ===$/m",
+        "/^==== (.*?) ====$/m",
+      ),
+      array(
+        "<a href=\"$1\">$1</a>$2", 
+        "<h2>$1</h2>",
+        "<h3>$1</h3>",
+        "<h4>$1</h4>",
+        "<h5>$1</h5>"
+      ), 
+      $text);
+    $text = str_replace("\n", "<br />\n", $text);
+    // But web convention is \r\n
+    $text = str_replace("\n", "\r\n", $text);
+    return $text;
+}
